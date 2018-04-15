55ec79c2f93ec413cb24c3d780906cfb2e741a3d
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-04-15 17:51:18 +0000

    Student Directory - Complete

--- directory.rb
+++ directory.rb
@@ -1,18 +1,13 @@
+require 'csv'
+
 @students = [] # an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    # get another name from the user
-    name = gets.chomp
-  end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
 end
 
 def interactive_menu
@@ -25,26 +20,57 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def process(selection)
+  case selection
+    when "1"
+      puts "You chose to add student"
+      input_students
+    when "2"
+      puts "You chose to display student list"
+      show_students
+    when "3"
+      puts "You chose to save the students to the file- enter the name of the file or default will be used"
+      filename = STDIN.gets.chomp
+      save_students(filename)
+    when "4"
+      puts "You chose to load student list from the file"
+      filename = STDIN.gets.chomp
+      load_students(filename)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just enter return twice"
+  name = STDIN.gets.chomp
+
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+def add_student(name, cohort = "november")
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def show_students
   print_header
   print_student_list
   print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit # this will cause the program to terminate
-  else
-    puts "I don't know what you meant, try again"
-  end
 end
 
 def print_header
@@ -60,6 +86,37 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students(filename)
+  if filename == ""
+    filename = "students.csv"
+  end
+  CSV.open("#{filename}", "w") do |csv| 
+     @students.each do |student|
+     student_data = [student[:name], student[:cohort]]
+     csv << student_data
+     end
+   end
+end
+  
+def load_students(filename)
+  if !File.exists?(filename)
+    filename = "students.csv"
+  end
+  CSV.foreach("#{filename}", "r") do |row|
+    name, cohort = row
+    add_student(name, cohort.to_sym)
+  end
+end
+
+def try_load_students
+  filename = ARGV.first
+  if filename.nil?
+    filename = "students.csv"
+  end
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
 end
 
 interactive_menu
